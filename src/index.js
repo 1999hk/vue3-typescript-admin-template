78028962df6fa866766f6a241b19cd641938aa67
1999hk <18169791243@163.com>
2022-04-14 00:12:23 +0800

    feat(loader): create a style、css、less-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
-import bar from './a'
-bar()
+// import bar from './a'
+// import css from './style/index.less'
+// bar()
+console.log('hello webpack');
+// console.log('ddd')
+// console.log('bbb')
 
-console.log('index');
+// import { createApp } from 'vue'
+
+// const Counter = {
+//   data() {
+//     return {
+//       counter: 0
+//     }
+//   },
+//   mounted() {
+//     setInterval(() => {
+//       this.counter++
+//     }, 1000)
+//   }
+// }
+
+// createApp(Counter).mount('#counter')
+
+// 起一个新项目
+// 单个前端，还是团伙
+// 安装依赖问题
+// 样式 静态资源（图片第三字体 es6+ vue ts）
+// 样式：借助css-loader 处理css语法，借助style-loader 使用css
+
+// 自定义loader的实现-> 如何编写一个loader
+
+// loader本质就是一个函数
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,82 @@
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
 module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
 
-  mode: 'development',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
 
-  
+  // 美化自定义输入的loader
+  resolveLoader: {
+    // 先去node_modules去找loader 找不到就去./myLoaders找
+    modules: ['node_modules', "./myLoaders"]
+  },
+
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader']
+      // },
+      {
+        test: /\.less$/,
+        // use: [
+        //   // 把css抽离成独立的css文件
+        //   MiniCssExtractPlugin.loader,
+        //   {
+        //     loader: 'css-loader',
+        //     // options: {}
+        //   },
+        //   'postcss-loader',
+        //   'less-loader'
+        // ]
+        use: ['lin-style-loader', 'lin-css-loader', 'lin-less-loader']
+      },
+      // 测试myLoaders
+      {
+        test: /\.js$/,
+        // use: { // 执行单个loader
+        //   loader: path.resolve(__dirname, './myLoaders/replace-loader.js'), // 要使用绝对路径
+        //   options: {
+        //     info: 'lin'
+        //   }
+        // }
+        use: [ // 执行多个loader
+          // path.resolve(__dirname, './myLoaders/replace-loader.js'), // 要使用绝对路径
+          // {
+          //   loader: path.resolve(__dirname, './myLoaders/replace-async-loader.js'), // 要使用绝对路径
+          //   options: {
+          //     info: 'world'
+          //   }
+          // }
+          'replace-loader', // 要使用绝对路径
+          // {
+          'replace-async-loader', // 要使用绝对路径
+            // options: {
+            //   info: 'world'
+            // }
+          // }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    // 仅对production模式有效
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ]
 
 }
